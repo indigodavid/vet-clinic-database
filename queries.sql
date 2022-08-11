914cd1fd939fa64f09647fc8cec6a7e42cb7e267
David Vera Castillo <indigodavid@gmail.com>
2022-08-10 23:16:46 -0400

    Add day 2 queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+SELECT avg(weight_kg) FROM animals;
+SELECT sum(escape_attempts), neutered 
+FROM animals GROUP BY neutered ORDER BY sum(escape_attempts) DESC limit 1;
+SELECT min(weight_kg), max(weight_kg) FROM animals;
+SELECT species, avg(escape_attempts) FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
